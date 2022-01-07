d0b68f0814596200a66cd02a5145e5d9d57373de
orabampho <orabampho@outlook.com>
2022-01-07 10:24:21 -0500

    add comment exercise 1.3

--- week-1/rabampho-exercise1.3.js
+++ week-1/rabampho-exercise1.3.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Professor Krasso
+; Date: 7 January 2022
+; Modified By: Orawan Rabampho
+; Code Attribution: Exercise 1.3 assignment instructions
+;===========================================
+*/
+
 var url = require("url");
 var parsedURL = url.parse("https://www.example.com/profile?name=smith");
 
