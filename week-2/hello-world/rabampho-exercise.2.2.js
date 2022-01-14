11a6c9e800947425c0bd3b4655da278ab9b0b7f8
orabampho <orabampho@outlook.com>
2022-01-14 09:10:02 -0500

    add comments

--- week-2/hello-world/rabampho-exercise.2.2.js
+++ week-2/hello-world/rabampho-exercise.2.2.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title: Exercise 2.2
+; Author: Professor Krasso
+; Date: 14 January 2022
+; Modified By: Orawan Rabampho
+; Code Attribution: Exercise 2.2 assignment instructions
+;===========================================
+*/
+
 var express = require("express");
 var http = require("http");
 
