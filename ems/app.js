6532dad12c0636ab40a14a2dc673813714ffb465
orabampho <orabampho@outlook.com>
2022-02-19 22:37:45 -0500

    add mongoose connection to mongoDB

--- ems/app.js
+++ ems/app.js
@@ -1,17 +1,66 @@
+/*
+=================================================
+Title: Assignment EMS 
+Author: Professor Krasso
+Date 19 February 2022
+Modified By: Orawan Rabampho
+Description: EMS
+=================================================
+*/
+
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 
+//required for mongoose
+var mongoose = require("mongoose");
+var Employee = require("./models/employee");
+
+//mLab connection
+var mongoDB = "mongodb+srv://admin:EmP10y33@buwebdev-cluster-1.ogmhj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+//mongoose connection to MongoDB
+mongoose.connect(mongoDB, {
+
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error."));
+db.once("open", function(){
+    console.log("Application connected to MongoDB Atlas Cluster");
+});
+
 var app = express();
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("short"));
+
 app.get("/", function(request, response){
     response.render("index",{
         title: "Home page"
     });
 });
+
+app.get("/list", function(request, response){
+    response.render("list", {
+        title: "Tabular view of employee records",
+    });
+});
+
+app.get("/new", function(request, response){
+    response.render("new", {
+        title: "Data entry page",
+    });
+});
+
+app.get("/view", function (request, response){
+    response.render("view", {
+        title: "View selected employee details",
+    });
+});
+
 
 http.createServer(app).listen(8080, function(){
     console.log("Application started on port 8080!");
